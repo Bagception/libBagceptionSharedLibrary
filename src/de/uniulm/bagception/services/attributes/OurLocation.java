91c3e6cf8b3c36311a9709a73fe6d90d40b1c84a
Steffen Herrdum <steffen.herrdum@uni-ulm.de>
2014-01-16 13:50:52 +0100

    added global location attributes

---
+++ src/de/uniulm/bagception/services/attributes/OurLocation.java
@@ -0,0 +1,19 @@
+package de.uniulm.bagception.services.attributes;
+
+public class OurLocation {
+	
+	public static final String PROVIDER = "provider";
+	public static final String LATITUDE = "latitude";
+	public static final String LONGITUDE = "longitude";
+	public static final String ACCURACY = "accuracy";
+	public static final String LOCATION_ACCURACY_THRESHOLD = "locationAccuracyThreshold";
+	public static final String REQUEST_TYPE = "requestType";
+	public static final String RESPONSE_TYPE = "responseType";
+	public static final String LOCATION = "location";
+	public static final String RESOLVE_ADDRESS = "resolveAddress";
+	
+	public static final String NETWORK = "network";
+	public static final String GPS = "gps";
+	public static final String BLUETOOTH = "bluetooth";
+	public static final String NONE = "none";
+}
