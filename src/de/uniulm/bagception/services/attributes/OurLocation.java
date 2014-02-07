9ec0a387e8f7b87f0e72d14bf7529166d04ed979
Steffen Herrdum <steffen.herrdum@uni-ulm.de>
2014-02-07 13:06:45 +0100

    added comments to location attributes

--- src/de/uniulm/bagception/services/attributes/OurLocation.java
+++ src/de/uniulm/bagception/services/attributes/OurLocation.java
@@ -2,24 +2,125 @@ package de.uniulm.bagception.services.attributes;
 
 public class OurLocation {
 	
-	public static final String PROVIDER = "provider";
-	public static final String LATITUDE = "latitude";
-	public static final String LONGITUDE = "longitude";
-	public static final String ACCURACY = "accuracy";
+	/**
+	 * Maximal aberration of a locations position in m.
+	 */
 	public static final String LOCATION_ACCURACY_THRESHOLD = "locationAccuracyThreshold";
+	
+	
+	
+	/**
+	 * Defines the type of a request. MUST be defined!
+	 * <br>1. GETLOCATION for a location request
+	 * <br>2. RESOLVEADDRESS resolves an address to lat long coordinates
+	 */
 	public static final String REQUEST_TYPE = "requestType";
-	public static final String RESPONSE_TYPE = "responseType";
+	
+	
+	
+	/**
+	 * Requests a latitude and longitude position
+	 */
 	public static final String GETLOCATION = "getLocation";
+
+	
+	
+	/**
+	 * Resolves an address to latitude longitude coordinates
+	 */
+	public static final String RESOLVEADDRESS = "resolveAddress";
+	
+	
+	
+	/**
+	 * Resolves latitude and longitude coordinates to an address
+	 */
+	public static final String RESOLVECOORDS = "resolveCoords";
+	
+	/**
+	 * Address which shall be resolved to latitude and longitude coordinates
+	 */
+	public static final String ADDRESS = "address";
+	
+	
+	/**
+	 * Defines the type of a response as a request reply. MUST be defined!
+	 */
+	public static final String RESPONSE_TYPE = "responseType";
+	
+	
+	
+	/**
+	 * Defines the provider from which the request was resolved.
+	 */
+	public static final String PROVIDER = "provider";
+	
+	
+	
+	/**
+	 * Latitude position from an address, location or current position.
+	 */
+	public static final String LATITUDE = "latitude";
+	
+	
+	
+	/**
+	 * Longitude position from an address, location or current position.
+	 */
+	public static final String LONGITUDE = "longitude";
+	
+	
+	
+	/**
+	 * Accuracy of a found position in m.
+	 */
+	public static final String ACCURACY = "accuracy";
+	
+	
+	
+	/**
+	 * 
+	 */
 	public static final String LOCATION = "location";
 	
-	public static final String RESOLVE_ADDRESS = "resolveAddress";
 	
+	
+	/**
+	 * Request was resolved by the android network provider (cell).
+	 */
 	public static final String NETWORK = "network";
+	
+	
+	
+	/**
+	 * Request was resolved by the android gps provider.
+	 */
 	public static final String GPS = "gps";
+	
+	
+	
+	/**
+	 * Request was resolved by our own bluetooth provider.
+	 */
 	public static final String BLUETOOTH = "bluetooth";
+	
+	
+	
+	/**
+	 * Request was resolved by our own wifi provider.
+	 */
+	public static final String WIFI = "wiwi";
+	
+
+	/**
+	 * Request could not be resolved.
+	 */
 	public static final String NONE = "none";
 	
-	public static final String ADDRESS = "address";
-	public static final String MAC = "mac";
 	
+	
+	/**
+	 * Defines the mac address of a resolved location request.
+	 */
+	public static final String MAC = "mac";
 }
