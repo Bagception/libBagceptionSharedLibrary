44867b8700389ed44944b3858502bf18da22543a
Philipp Hock <philipp.hock@uni-ulm.de>
2014-02-21 14:41:26 +0100

    removed unused imports

--- src/de/uniulm/bagception/bundlemessageprotocol/BundleMessage.java
+++ src/de/uniulm/bagception/bundlemessageprotocol/BundleMessage.java
@@ -6,6 +6,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import android.os.Bundle;
+import de.uniulm.bagception.bundlemessageprotocol.BundleMessage.BUNDLE_MESSAGE;
 import de.uniulm.bagception.bundlemessageprotocol.entities.Item;
 
 public class BundleMessage {
